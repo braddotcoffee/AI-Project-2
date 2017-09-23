8060996972aad4b3818abcee3295a6b05e2011fa
bwbonanno <bwbonanno@wpi.edu>
2017-09-23 14:15:34 -0400

    Paramaterized Coeff

--- lib/brain.py
+++ lib/brain.py
@@ -5,8 +5,9 @@ from lib.board import Board
 from lib.color import Color
 
 class Brain(object):
-    def __init__(self, color=Color.WHITE):
+    def __init__(self, color=Color.WHITE, coeff=[1,3,5,7,9]):
         self._color = color
+        self._coeff = coeff
 
     # Chooses move to make
     # Returns Piece to add to board
@@ -16,11 +17,9 @@ class Brain(object):
     def evaluation_function(self, board):
         own_score = [ sets_of_adjacent(i, board.all_friendly())  for i in range(1,6) ]
         enemy_score = [ sets_of_adjacent(i, board.all_enemy())  for i in range(1,6) ]
-        score =  Brain.apply_coefficient(own_score[0], enemy_score[0], 1)
-        score += Brain.apply_coefficient(own_score[1], enemy_score[1], 3)
-        score += Brain.apply_coefficient(own_score[2], enemy_score[2], 5)
-        score += Brain.apply_coefficient(own_score[3], enemy_score[3], 7)
-        score += Brain.apply_coefficient(own_score[4], enemy_score[4], 9)
+        score = 0
+        for i in range(5):
+            score += Brain.apply_coefficient(own_score[i], enemy_score[i], self._coeff[i])
         return score
 
     @staticmethod
