84aac9fca02b3d91713f6448862d436ea5e7d535
Alex Taglieri <atags22@gmail.com>
2017-09-24 16:56:15 -0400

    Created file hands.py to act as File I/O and populated it with pseudocode

---
+++ lib/hands.py
@@ -0,0 +1,28 @@
+
+
+class Hands():
+
+    def __init__(self):
+
+    def main():
+        #loop:
+            #wait several ms
+            #poll for move_file
+            #if exists:
+                #init all the things
+        #loop:
+            #wait several ms
+            #poll for our file
+            #if exists:
+                #read move_file
+                #if we don't know what color we are:
+                    #if empty:
+                        #we are first to move
+                    #else
+                        #we're second to move
+                #pass last move to body
+
+    def write_move(move):
+        #open move_file
+        #append in our move
+        #close file
