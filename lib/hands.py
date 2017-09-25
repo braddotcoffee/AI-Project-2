cf8c40ed95d97ff0ef56e746a34c86981158924b
Alex Taglieri <atags22@gmail.com>
2017-09-25 01:50:38 -0400

    Work in progress (hands.py)

--- lib/hands.py
+++ lib/hands.py
@@ -1,20 +1,47 @@
-
+import time
+import os.path
 
 class Hands():
-
-    def __init__(self):
-
-    def main():
+    print("Made")
+    #def __init__(self):
+    if(True):
         #loop:
-            #wait several ms
-            #poll for move_file
+        started = False
+        pollNextMove=False
+        print("Hi")
+        while(started == False):
+            #wait several ms,
+            # then poll for move_file            
+            time.sleep(1)
+            #//TODO: Make the time delay lower
+
+            
             #if exists:
                 #init all the things
+            if(os.path.isfile("myFileName")):
+               started = True
+               pollNextMove = True
+               print ("yes")
+               #//TODO: Start all the things
+               
+
+
+        while(pollNextMove):
         #loop:
             #wait several ms
             #poll for our file
+            time.sleep(0.5)
+            if(os.path.isfile("move_file")):
             #if exists:
                 #read move_file
+               with open("move_file", "r") as file:
+                   for line in file:
+                       if not line: #file is empty
+                           print("empty")
+                           pollNextMove = false
+               
+               
+               
                 #if we don't know what color we are:
                     #if empty:
                         #we are first to move
@@ -22,7 +49,8 @@ class Hands():
                         #we're second to move
                 #pass last move to body
 
-    def write_move(move):
+#    def write_move(move):
         #open move_file
         #append in our move
         #close file
+
