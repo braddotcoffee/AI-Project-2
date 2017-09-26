f9a3dcbcca5876a82331501c23f5df32fd4ea05f
Alex Taglieri <atags22@gmail.com>
2017-09-26 18:51:43 -0400

    Refactored Hands to make only one loop instead of two

--- lib/hands.py
+++ lib/hands.py
@@ -8,57 +8,38 @@ class Hands():
     def __init__(self):
     
         #loop:
-        started = False
-        pollNextMove=False
+        pollNextMove = True
         myColor = Color.NONE
         
         print("Initialized")
-        while(started == False):
+        while(pollNextMove == True):
             #wait several ms,
             # then poll for move_file            
-            time.sleep(1)
+            time.sleep(2)
             #//TODO: Make the time delay lower
 
             
             #if exists:
                 #init all the things
             if(os.path.isfile("myFileName")):
-               started = True
-               pollNextMove = True
-               print ("Started")
+               print ("Our move!")
                #//TODO: Start all the things
-               
 
-
-        while(pollNextMove): #Run until game over received
-            print("Color: ",myColor)
-        #loop:
-            #wait several ms
-            #poll for our file
-            time.sleep(2)
             if(os.path.isfile("move_file")):
-            #if exists:
-                #read move_file
-               with open("move_file", "r") as file:
+
+                with open("move_file", "r") as file:
                    if(myColor == Color.NONE):            
+                       #This is the first move of the game
                        file.seek(0)
                        first_char = file.read(1)
                        if not first_char:
                            myColor = Color.WHITE
                        else:
-                            file.seek(0)
                             myColor = Color.BLACK
-                            
-                       
-                    #else:
-                     #  file.seek(0) #go back to beginning
-                   
-               
-                #if we don't know what color we are:
-                    #if empty:
-                        #we are first to move (white)
-                    #else
-                        #we're second to move (black)
+                       file.seek(0)
+                       #Initialize all the code here
+                    
+                    
                 #pass last move to body
 
 #    def write_move(move):
